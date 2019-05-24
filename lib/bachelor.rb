afd6a0744e01aa865f3eeb149f39599dee8b4a2b
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-24 11:29:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,7 +29,17 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |ss, contestants|
+    contestants.each do |contestant|
+      contestant.each do |info, value|
+        if value == hometown
+          count += 1
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
